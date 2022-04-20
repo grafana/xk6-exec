697bbb9d1ff4e327d7adfcd0990322212ec3eabf
javaducky <javaducky@gmail.com>
2022-04-20 14:23:35 -0500

    Addresses issues for #5 to rework module code

--- exec.go
+++ exec.go
@@ -2,18 +2,42 @@ package exec
 
 import (
 	"log"
-	"strings"
 	"os/exec"
+	"strings"
 
 	"go.k6.io/k6/js/modules"
 )
 
 func init() {
-	modules.Register("k6/x/exec", new(EXEC))
+	modules.Register("k6/x/exec", new(RootModule))
 }
 
-// EXEC is the k6 say extension.
-type EXEC struct{}
+// RootModule is the global module object type. It is instantiated once per test
+// run and will be used to create `k6/x/exec` module instances for each VU.
+type RootModule struct{}
+
+// EXEC represents an instance of the EXEC module for every VU.
+type EXEC struct {
+	vu modules.VU
+}
+
+// Ensure the interfaces are implemented correctly.
+var (
+	_ modules.Module   = &RootModule{}
+	_ modules.Instance = &EXEC{}
+)
+
+// NewModuleInstance implements the modules.Module interface to return
+// a new instance for each VU.
+func (*RootModule) NewModuleInstance(vu modules.VU) modules.Instance {
+	return &EXEC{vu: vu}
+}
+
+// Exports implements the modules.Instance interface and returns the exports
+// of the JS module.
+func (exec *EXEC) Exports() modules.Exports {
+	return modules.Exports{Default: exec}
+}
 
 // Command is a wrapper for Go exec.Command
 func (*EXEC) Command(name string, args []string) string {
